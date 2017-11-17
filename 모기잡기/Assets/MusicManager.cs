6738b0672fd7970c378b3221b3a4360908ee7d06
Myeongzin Son <sonmyungzin1@naver.com>
2017-11-18 06:00:08 +0900

    웅표 작업 추가

---
+++ 모기잡기/Assets/MusicManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    AudioSource audio;
+    public float asdf;
+    public GameObject standardFly;
+    // Use this for initialization
+
+    void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        audio.Play();
+    }
+
+    void Start () {
+        PlayMusic();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
