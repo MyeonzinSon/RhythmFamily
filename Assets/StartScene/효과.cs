9490eec21e183544daa08200801f1ed247ff106c
Myeongzin Son <sonmyungzin1@naver.com>
2017-11-20 00:15:42 +0900

    필요 자료 정리

---
+++ Assets/StartScene/효과.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 효과 : MonoBehaviour {
+
+    public float speed;
+    public float maxX;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + new Vector3(speed*Time.deltaTime,0,0);
+
+        if (transform.position.x > maxX)
+        {
+            transform.position = new Vector3(-1 * maxX,transform.position.y,0);
+        }
+	}
+}
---
+++ Assets/파리잡기/bugmove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bugmove : MonoBehaviour {
+
+    public float startTime;
+    public float checkTime;
+	// Use this for initialization
+	void Awake ()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        StartTime(startTime);
+    }
+    void StartTime(float time)
+    {
+        if (Time.time > time)
+        {
+        transform.position = new Vector3(transform.position.x, transform.position.y, -4);
+
+        GetComponent<AudioSource>().Play();
+        }
+       
+      
+        CheckTime(checkTime);
+	}
+    void CheckTime(float time)
+    {
+        if (Time.time > time)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 1);
+         
+        }
+    }
+}
